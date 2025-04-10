78f1c34ba0a4a64173ea6625b967cf4b883fbc4b
sandykr29 <sandeepkumar29774@yahoo.com>
2025-04-11 01:36:52 +0530

    hero section done

--- appscrip/app/components/Header.js
+++ appscrip/app/components/Header.js
@@ -16,10 +16,15 @@ const Header = () => {
      <div className={styles.topContainer}>
 
         <div className={styles.logo}>
+        <div className={styles.hamburger} onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
           <img src="/logo.png" alt="Logo-icon" width="36" height="36" />
         </div>
 
         <h1>LOGO</h1>
+       
+
 
         <div className={styles.actions}>
           <span>
@@ -31,10 +36,10 @@ const Header = () => {
           <span>
             <Image src="/shopping-bag.png" alt="Shopping Bag Icon" width={24} height={24} />
           </span>
-          <span>
+          <span className={styles.onHide}>
             <Image src="/profile.png" alt="Profile Icon" width={24} height={24} />
           </span>
-          <span>ENG ▼</span>
+          <span className={styles.onHide}>ENG ▼</span>
         </div>
 
       </div>
@@ -44,11 +49,11 @@ const Header = () => {
 
 <div className={styles.bottomContainer}>
    <nav className={`${styles.nav} ${menuOpen ? styles.open : ''}`}>
-          <a href="#">SHOP</a>
-          <a href="#">SKILLS</a>
-          <a href="#">STORIES</a>
-          <a href="#">ABOUT</a>
-          <a href="#">CONTACT US</a>
+          <a href="#" onClick={() => setMenuOpen(false)}>SHOP</a>
+          <a href="#" onClick={() => setMenuOpen(false)}>SKILLS</a>
+          <a href="#" onClick={() => setMenuOpen(false)}>STORIES</a>
+          <a href="#" onClick={() => setMenuOpen(false)}>ABOUT</a>
+          <a href="#" onClick={() => setMenuOpen(false)}>CONTACT US</a>
         </nav>
 
 
